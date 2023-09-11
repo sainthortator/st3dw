123da39daa00cc1c3f1b80a747a9c212c8e89d18
sainthortator <adamshumag@gmail.com>
2023-09-11 21:40:29 +0300

    Import test model

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const THREE = await import('./three.js');
+const { MMDLoader } = await import('./MMDLoader.js');
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({
+	antialias: true,
+	alpha: true
+});
 
-renderer.setClearColor(0xffffff, 1);
-renderer.setSize(500, 500);
+renderer.setClearColor(0x000000, 0);
+renderer.setSize(620, 620);
 
 const container = $('#expression-holder');
 container.css('display', '');
@@ -11,12 +15,22 @@ container.append(renderer.domElement);
 
 $('#expression-image').css('display', 'none');
 
-const camera = new THREE.PerspectiveCamera(70, 1);
-camera.position.z = 0;
-camera.position.y = 0;
+const camera = new THREE.PerspectiveCamera(50, 1);
+camera.position.z = 7;
+camera.position.y = 17;
 
 const scene = new THREE.Scene();
 scene.add(camera);
+
+const loader = new MMDLoader();
+
+loader.load('scripts/extensions/st3dw/test_model/model.pmx', mesh => {
+    scene.add(mesh);
+});
+
+const light = new THREE.AmbientLight(0x404040);
+light.intensity = 60;
+scene.add(light);
 
 function animate() {
 	requestAnimationFrame(animate);
