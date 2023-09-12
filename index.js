6438a39a33857f7c285bd1be10bbc127eae159ed
sainthortator <adamshumag@gmail.com>
2023-09-12 03:15:24 +0300

    Add a morphs debug function

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const THREE = await import('./modules/three.js');
 const { MMDLoader } = await import('./modules/MMDLoader.js');
 const { MMDAnimationHelper } = await import('./modules/MMDAnimationHelper.js');
-const { Clock } = await import('./modules/three.js');
 const { OutlineEffect } = await import('./modules/OutlineEffect.js');
 
 const clock = new THREE.Clock();
@@ -38,8 +37,6 @@ loader.loadWithAnimation(
 		const mesh = mmd.mesh;
 		scene.add(mesh);
 
-		// console.log(mesh.morphTargetDictionary);
-
 		animHelper.add(mesh, {
 			animation: mmd.animation,
 			physics: true
@@ -54,6 +51,28 @@ function animate() {
 	requestAnimationFrame(animate);
 	animHelper.update(clock.getDelta());
 	outlineEffect.render(scene, camera);
+}
+
+window.enableMorphsDebug = function() {
+	const mesh = animHelper.meshes[0];
+
+	const morphsList = document.createElement('ol');
+	morphsList.classList.add('morphsList');
+	morphsList.setAttribute('start', 0);
+
+	mesh.morphTargetInfluences.forEach(() => {
+		morphsList.insertAdjacentHTML('beforeend', `
+			<li>
+				<input type="range" value="0" max="0.99" min="0" step="0.01">
+			</li>
+		`);
+	});
+	
+	document.body.append(morphsList);
+
+	document.querySelectorAll('.morphsList input').forEach((elem, index) => elem.addEventListener('input', evt => {
+		mesh.morphTargetInfluences[index] = +evt.target.value;
+	}));
 }
 
 animate();
